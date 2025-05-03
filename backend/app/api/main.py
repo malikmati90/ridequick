837680aef41d35e6924dffaf6e506f136711e344
Malik Mati <malikmati90@gmail.com>
2025-05-03 18:45:57 +0200

    Added crud functions and endpoints for vehicle features

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,12 +1,18 @@
 """ Main API routes definition """
 from fastapi import APIRouter
 
-from app.api.routes import login, users, utils, drivers
+from app.api.routes import (
+    login,
+    users,
+    utils,
+    drivers,
+    vehicles,
+)
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(drivers.router, prefix="/drivers", tags=["drivers"])
-# api_router.include_router(utils.router, prefix="/vehicles", tags=["vehicles"])
+api_router.include_router(vehicles.router, prefix="/vehicles", tags=["vehicles"])
 # api_router.include_router(utils.router, prefix="/bookings", tags=["bookings"])
---
+++ backend/app/crud/vehicle.py
@@ -0,0 +1,41 @@
+from typing import List, Optional
+from sqlmodel import Session, select, func
+
+from app.models import (
+    Vehicle,
+    VehicleCreate,
+    VehicleUpdate,
+    VehiclesOut
+)
+
+
+def create_vehicle(*, session: Session, vehicle_in: VehicleCreate) -> Vehicle:
+    vehicle = Vehicle.model_validate(vehicle_in)
+    session.add(vehicle)
+    session.commit()
+    session.refresh(vehicle)
+    return vehicle
+
+
+def read_vehicle_by_id(session: Session, vehicle_id: int) -> Optional[Vehicle]:
+    return session.get(Vehicle, vehicle_id)
+
+
+def read_vehicles(*, session: Session, skip: int = 0, limit: int = 100) -> VehiclesOut:
+    count = session.exec(select(func.count()).select_from(Vehicle)).one()
+    vehicles = session.exec(select(Vehicle).offset(skip).limit(limit)).all()
+    return VehiclesOut(data=vehicles, count=count)
+
+
+def update_vehicle(*, session: Session, db_vehicle: Vehicle, vehicle_in: VehicleUpdate) -> Vehicle:
+    vehicle_data = vehicle_in.model_dump(exclude_unset=True)
+    db_vehicle.sqlmodel_update(vehicle_data)
+    session.add(db_vehicle)
+    session.commit()
+    session.refresh(db_vehicle)
+    return db_vehicle
+
+
+def delete_vehicle(*, session: Session, db_vehicle: Vehicle):
+    session.delete(db_vehicle)
+    session.commit()
